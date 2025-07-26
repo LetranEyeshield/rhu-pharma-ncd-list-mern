1c6de00e57904fc521beb5918da2845f0976bccc
YORIICHI <rosalinmichaelcris@gmail.com>
2025-07-26 22:47:57 +0800

    changed the axiosInstance

--- src/services/api.ts
+++ src/services/api.ts
@@ -165,17 +165,16 @@ export const loginUser = async (
 }> => {
   try {
     // const res = await fetch("http://localhost:5000/api/auth/login", {
-    // const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
-    const res = await fetch(
-      "https://rhu-pharma-ncd-list-mern.onrender.com/api/auth/login",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      }
-    );
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+      // const res = await fetch(
+      //   "https://rhu-pharma-ncd-list-mern.onrender.com/api/auth/login",
+      //   {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
 
     const data = await res.json();
 
